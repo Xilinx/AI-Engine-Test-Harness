dae08c1ee81c828f58df6f8478f8b87e6620f730
Tuo Lin <tuol@xilinx.com>
2023-04-03 19:15:01 -0700

    add missing 'MIT License'

--- examples/super-sampling-rate-fir/SingleStreamSSR/ps/host.cpp
+++ examples/super-sampling-rate-fir/SingleStreamSSR/ps/host.cpp
@@ -1,4 +1,6 @@
-/**
+/*
+ * MIT License
+ *
  * Copyright (C) 2023 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
@@ -13,7 +15,7 @@
  * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ */
 
 //#include <stdio.h>
 //#include <stdlib.h>
--- src/aie/dummy_graph.cpp
+++ src/aie/dummy_graph.cpp
@@ -1,4 +1,6 @@
-/**
+/*
+ * MIT License
+ *
  * Copyright (C) 2023 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
