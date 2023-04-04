c1340c744a749fbd085c54e572ea95a9350f1b54
Tuo Lin <tuol@xilinx.com>
2023-04-03 18:46:18 -0700

    [1] Update LICENCE to MIT LICENCE [2] Update Copyright to Advanced Micro Devices, Inc

--- examples/super-sampling-rate-fir/SingleKernel/aie/aie_kernels/FirSingleStream.h
+++ examples/super-sampling-rate-fir/SingleKernel/aie/aie_kernels/FirSingleStream.h
@@ -1,47 +1,41 @@
 /*
- * (c) Copyright 2021-2022 Xilinx, Inc. All rights reserved.
+ * Copyright (C) 2023 Advanced Micro Devices, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the “Software”), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
- #pragma once
+#pragma once
 
 #include <adf.h>
 
 namespace SingleStream {
 
-template<int NSamples,int ShiftAcc>
+template <int NSamples, int ShiftAcc>
 class FIR_SingleStream {
-private:
-	alignas(32) cint16 weights[32];
-	alignas(32) cint16 delay_line[32];
+   private:
+    alignas(32) cint16 weights[32];
+    alignas(32) cint16 delay_line[32];
 
-public:
-	FIR_SingleStream(const cint16 (&taps)[32])
-	{
-		for(int i=0;i<32;i++)
-		{
-			weights[i] = taps[i];
-			delay_line[i] = (cint16){0,0};
-		}
-	};
+   public:
+    FIR_SingleStream(const cint16 (&taps)[32]) {
+        for (int i = 0; i < 32; i++) {
+            weights[i] = taps[i];
+            delay_line[i] = (cint16){0, 0};
+        }
+    };
 
-	void filter(input_stream_cint16*  sin,output_stream_cint16*  sout);
+    void filter(input_stream_cint16* sin, output_stream_cint16* sout);
 
-	static void registerKernelClass()
-	{
-		REGISTER_FUNCTION(FIR_SingleStream::filter);
-	};
+    static void registerKernelClass() { REGISTER_FUNCTION(FIR_SingleStream::filter); };
 };
-
 }
