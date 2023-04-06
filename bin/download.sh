10fa6f5b0f1b9afe1f1e15cdfc4a56d9a96c13ed
Tuo Lin <tuo.lin@xilinx.com>
2023-04-06 13:19:18 +0800

    Update MIT license to MIT X11 license (#16)
    Co-authored-by: Tuo Lin <tuol@xilinx.com>

--- bin/download.sh
+++ bin/download.sh
@@ -9,5 +9,7 @@
 #
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+# Except as contained in this notice, the name of Advanced Micro Devices, Inc. shall not be used in advertising or otherwise to promote the sale, use or other dealings in this Software without prior written authorization from Advanced Micro Devices, Inc.
+#
 
 echo "This is script that lead user to downloading url of pre-compiled .xsa"
