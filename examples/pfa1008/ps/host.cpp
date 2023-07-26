a1bee13689f705f9735cf44506d06117a928b046
leol <leol@xilinx.com>
2023-07-26 17:58:11 -0600

    Result checking for PFA1008

--- examples/pfa1008/ps/host.cpp
+++ examples/pfa1008/ps/host.cpp
@@ -21,16 +21,6 @@
  * Advanced Micro Devices, Inc.
  */
 
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <stdint.h>
-//#include <fstream>
-
-#include "vck190_test_harness_mgr.hpp"
-
-// #define DATA_SIZE 16
-
-using namespace vck190_test_harness;
 
 #include <string>
 #include <vector>
@@ -39,49 +29,113 @@ using namespace vck190_test_harness;
 #include <sstream>
 #include <cstring>
 
+#include "vck190_test_harness_mgr.hpp"
+
+#include "utils.hpp" // Utilities (read_data_from_file, check_size...)
+
+typedef std::complex<int16_t> cint16_t;
+
+using namespace vck190_test_harness;
+
 using namespace std;
 
 int main(int argc, char** argv) {
     // Prepare data
-    const int in_ch = 6;
-    const int out_ch = 6;
-    int in_sz = 1008 * 16; // total number of elements * sizeof(int128)
-    int out_sz = 1008 * 16; // total number of elements * sizeof(int128)
-    char* in_data[in_ch];
-    char* out_data[out_ch];
+    int in_sz = 1008 * 16;
+    int out_sz = 1008 * 16;
 
-    for (int i = 0; i < in_ch; i++) {
-        in_data[i] = (char*)malloc(in_sz);
-    }
-    for (int i = 0; i < out_ch; i++) {
-        out_data[i] = (char*)malloc(out_sz);
-    }
+    std::vector<cint16_t> in_dft7_0;
+    std::vector<cint16_t> in_dft7_1;
+    std::vector<cint16_t> in_dft9_0;
+    std::vector<cint16_t> in_dft9_1;
+    std::vector<cint16_t> in_dft16_0;
+    std::vector<cint16_t> in_dft16_1;
+    std::vector<cint16_t> out_dft7_0;
+    std::vector<cint16_t> out_dft7_1;
+    std::vector<cint16_t> out_dft9_0;
+    std::vector<cint16_t> out_dft9_1;
+    std::vector<cint16_t> out_dft16_0;
+    std::vector<cint16_t> out_dft16_1;
+    std::vector<cint16_t> golden_dft7_0;
+    std::vector<cint16_t> golden_dft7_1;
+    std::vector<cint16_t> golden_dft9_0;
+    std::vector<cint16_t> golden_dft9_1;
+    std::vector<cint16_t> golden_dft16_0;
+    std::vector<cint16_t> golden_dft16_1;
+
+    read_data_from_file("./data/dft7_i_0.txt",  in_dft7_0);
+    read_data_from_file("./data/dft7_i_1.txt",  in_dft7_1);
+    read_data_from_file("./data/dft9_i_0.txt",  in_dft9_0);
+    read_data_from_file("./data/dft9_i_1.txt",  in_dft9_1);
+    read_data_from_file("./data/dft16_i_0.txt", in_dft16_0);
+    read_data_from_file("./data/dft16_i_1.txt", in_dft16_1);
+    read_data_from_file("./data/dft7_o_0.txt", golden_dft7_0);
+    read_data_from_file("./data/dft7_o_1.txt", golden_dft7_1);
+    read_data_from_file("./data/dft9_o_0.txt", golden_dft9_0);
+    read_data_from_file("./data/dft9_o_1.txt", golden_dft9_1);
+    read_data_from_file("./data/dft16_o_0.txt", golden_dft16_0);
+    read_data_from_file("./data/dft16_o_1.txt", golden_dft16_1);
+    out_dft7_0.resize(golden_dft7_0.size());
+    out_dft7_1.resize(golden_dft7_1.size());
+    out_dft9_0.resize(golden_dft9_0.size());
+    out_dft9_1.resize(golden_dft9_1.size());
+    out_dft16_0.resize(golden_dft16_0.size());
+    out_dft16_1.resize(golden_dft16_1.size());
+
     // run test with test harness
     test_harness_mgr mgr(0, argv[1], {"aie_dut"});
     std::vector<test_harness_args> args;
-    args.push_back({channel_index(PLIO_01_TO_AIE), in_sz, 1, 0, (char*)in_data[0]});
-    args.push_back({channel_index(PLIO_03_TO_AIE), in_sz, 1, 0, (char*)in_data[1]});
-    args.push_back({channel_index(PLIO_05_TO_AIE), in_sz, 1, 0, (char*)in_data[2]});
-    args.push_back({channel_index(PLIO_07_TO_AIE), in_sz, 1, 0, (char*)in_data[3]});
-    args.push_back({channel_index(PLIO_09_TO_AIE), in_sz, 1, 0, (char*)in_data[4]});
-    args.push_back({channel_index(PLIO_11_TO_AIE), in_sz, 1, 0, (char*)in_data[5]});
-    args.push_back({channel_index(PLIO_02_FROM_AIE), out_sz, 1, 0, (char*)out_data[0]});
-    args.push_back({channel_index(PLIO_04_FROM_AIE), out_sz, 1, 0, (char*)out_data[1]});
-    args.push_back({channel_index(PLIO_06_FROM_AIE), out_sz, 1, 0, (char*)out_data[2]});
-    args.push_back({channel_index(PLIO_08_FROM_AIE), out_sz, 1, 0, (char*)out_data[3]});
-    args.push_back({channel_index(PLIO_10_FROM_AIE), out_sz, 1, 0, (char*)out_data[4]});
-    args.push_back({channel_index(PLIO_12_FROM_AIE), out_sz, 1, 0, (char*)out_data[5]});
+    args.push_back({channel_index(PLIO_01_TO_AIE), in_sz, 1, 0, (char*)in_dft7_0.data()});
+    args.push_back({channel_index(PLIO_03_TO_AIE), in_sz, 1, 0, (char*)in_dft9_0.data()});
+    args.push_back({channel_index(PLIO_05_TO_AIE), in_sz, 1, 0, (char*)in_dft16_0.data()});
+    args.push_back({channel_index(PLIO_07_TO_AIE), in_sz, 1, 0, (char*)in_dft7_1.data()});
+    args.push_back({channel_index(PLIO_09_TO_AIE), in_sz, 1, 0, (char*)in_dft9_1.data()});
+    args.push_back({channel_index(PLIO_11_TO_AIE), in_sz, 1, 0, (char*)in_dft16_1.data()});
+
+    args.push_back({channel_index(PLIO_02_FROM_AIE), out_sz, 1, 0, (char*)out_dft7_0.data()});
+    args.push_back({channel_index(PLIO_04_FROM_AIE), out_sz, 1, 0, (char*)out_dft9_0.data()});
+    args.push_back({channel_index(PLIO_06_FROM_AIE), out_sz, 1, 0, (char*)out_dft16_0.data()});
+    args.push_back({channel_index(PLIO_08_FROM_AIE), out_sz, 1, 0, (char*)out_dft7_1.data()});
+    args.push_back({channel_index(PLIO_10_FROM_AIE), out_sz, 1, 0, (char*)out_dft9_1.data()});
+    args.push_back({channel_index(PLIO_12_FROM_AIE), out_sz, 1, 0, (char*)out_dft16_1.data()});
+
     mgr.runAIEGraph(0, 8);
     mgr.runTestHarness(args);
     mgr.waitForRes(10000);
 
-    //
-    for (int i = 0; i < in_ch; i++) {
-        free(in_data[i]);
+    int NUM_SAMPLES_O = out_sz/4; // Each sample is 4 bytes (32-bits or cint16)
+	int errorCount = 0;
+    {
+        for (int i = 0; i < NUM_SAMPLES_O; i++) {
+			int16_t err_re_dft7_0 = abs(real(golden_dft7_0[i]) - real(out_dft7_0[i]));
+			int16_t err_im_dft7_0 = abs(imag(golden_dft7_0[i]) - imag(out_dft7_0[i]));
+			int16_t err_re_dft7_1 = abs(real(golden_dft7_1[i]) - real(out_dft7_1[i]));
+			int16_t err_im_dft7_1 = abs(imag(golden_dft7_1[i]) - imag(out_dft7_1[i]));
+			int16_t err_re_dft9_0 = abs(real(golden_dft9_0[i]) - real(out_dft9_0[i]));
+			int16_t err_im_dft9_0 = abs(imag(golden_dft9_0[i]) - imag(out_dft9_0[i]));
+			int16_t err_re_dft9_1 = abs(real(golden_dft9_1[i]) - real(out_dft9_1[i]));
+			int16_t err_im_dft9_1 = abs(imag(golden_dft9_1[i]) - imag(out_dft9_1[i]));
+			int16_t err_re_dft16_0 = abs(real(golden_dft16_0[i]) - real(out_dft16_0[i]));
+			int16_t err_im_dft16_0 = abs(imag(golden_dft16_0[i]) - imag(out_dft16_0[i]));
+			int16_t err_re_dft16_1 = abs(real(golden_dft16_1[i]) - real(out_dft16_1[i]));
+			int16_t err_im_dft16_1 = abs(imag(golden_dft16_1[i]) - imag(out_dft16_1[i]));
+            if ( ( err_re_dft7_0 > 5 ) || ( err_im_dft7_0 > 5 ) || ( err_re_dft7_1 > 5 ) || ( err_im_dft7_1 > 5 ) || 
+			     ( err_re_dft9_0 > 5 ) || ( err_im_dft9_0 > 5 ) || ( err_re_dft9_1 > 5 ) || ( err_im_dft9_1 > 5 ) || 
+				 ( err_re_dft16_0 > 5 ) || ( err_im_dft16_0 > 5 ) || ( err_re_dft16_1 > 5 ) || ( err_im_dft16_1 > 5 ) )  {  // Matlab is not bit accurate
+                printf("Error found @ %d\n", i);
+                printf("dft7_0 (%d,%d) != (%d,%d)\n", real(out_dft7_0[i]), imag(out_dft7_0[i]), real(golden_dft7_0[i]), imag(golden_dft7_0[i]));
+                printf("dft7_1 (%d,%d) != (%d,%d)\n", real(out_dft7_1[i]), imag(out_dft7_1[i]), real(golden_dft7_1[i]), imag(golden_dft7_1[i]));
+                printf("dft9_0 (%d,%d) != (%d,%d)\n", real(out_dft9_0[i]), imag(out_dft9_0[i]), real(golden_dft9_0[i]), imag(golden_dft9_0[i]));
+                printf("dft9_1 (%d,%d) != (%d,%d)\n", real(out_dft9_1[i]), imag(out_dft9_1[i]), real(golden_dft9_1[i]), imag(golden_dft9_1[i]));
+                printf("dft16_0 (%d,%d) != (%d,%d)\n", real(out_dft16_0[i]), imag(out_dft16_0[i]), real(golden_dft16_0[i]), imag(golden_dft16_0[i]));
+                printf("dft16_1 (%d,%d) != (%d,%d)\n", real(out_dft16_1[i]), imag(out_dft16_1[i]), real(golden_dft16_1[i]), imag(golden_dft16_1[i]));
+                errorCount++;
+            }
+        }
+        if (errorCount)
+            printf("Test failed with %d errors\n", errorCount);
+        else
+            printf("TEST PASSED\n");
     }
-    for (int i = 0; i < out_ch; i++) {
-        free(out_data[i]);
-    }
-
-    return 0;
+    return errorCount;
 }
